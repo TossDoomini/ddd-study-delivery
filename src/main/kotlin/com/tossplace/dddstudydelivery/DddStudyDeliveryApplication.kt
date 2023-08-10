7b0f0e0e8f8c331a8b09b0ca171a43a617555007
Doomini <doomini@toss.im>
2023-08-10 15:35:55 +0900

    init

---
+++ src/main/kotlin/com/tossplace/dddstudydelivery/DddStudyDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package com.tossplace.dddstudydelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddStudyDeliveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<DddStudyDeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/tossplace/dddstudydelivery/DddStudyDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tossplace.dddstudydelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddStudyDeliveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
